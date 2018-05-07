5b43e203988b928428b2195243cd0b9456cdab7b
Alex <terra@fastmail.com>
2018-05-07 19:31:33 +0200

    WIP(EventGraphs): add graph template import

--- NDMC.Web.React/app/js/index.jsx
+++ NDMC.Web.React/app/js/index.jsx
@@ -21,7 +21,7 @@ import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Home from './components/Base/Home.jsx'
 import Events from './components/Events/List/Events.jsx'
 import EventDetails from './components/Events/Details/EventDetails.jsx'
-//import Graphs from './components/Events/Graphs/EventGraph.jsx'
+import Graphs from './components/Events/Graphs/EventGraph.jsx'
 import CustomNavbar from './components/Base/CustomNavbar.jsx'
 import { stripURLParam } from "./globalFunctions.js"
 
@@ -63,7 +63,7 @@ class App extends React.Component {
               <Route path="/" component={Home} exact />
               <Route path="/events" component={Events} exact />
               <Route path="/events/:id"component={EventDetails} exact />
-              {/* <Route path="/graphs" component={Graphs} exact /> */}
+              <Route path="/graphs" component={Graphs} exact />
             </Switch>
           </div>
         </Router>
