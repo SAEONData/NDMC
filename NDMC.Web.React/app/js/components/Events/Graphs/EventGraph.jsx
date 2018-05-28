980dfbbb7009f00fc6522ab675b257108651bf9c
terrahop <terra@fastmail.com>
2018-05-28 13:27:23 +0200

    refactor(EVentGraphs): change double quotes to single quotes

--- NDMC.Web.React/app/js/components/Events/Graphs/EventGraph.jsx
+++ NDMC.Web.React/app/js/components/Events/Graphs/EventGraph.jsx
@@ -1,11 +1,11 @@
 'use strict'
 
 import React from 'react'
-import EventFilters from '../Filters/EventFilters.jsx'
+//import EventGraphFilters from './EventGraphFilters.jsx'
 import { connect } from 'react-redux'
 import { BeatLoader } from 'react-spinners'
 import { Button, Footer, Container } from 'mdbreact'
-import * as ACTION_TYPES from "../../../constants/action-types"
+import * as ACTION_TYPES from '../../../constants/action-types'
 
 const queryString = require('query-string')
 
@@ -26,18 +26,17 @@ class EventGraph extends React.Component {
   render() {
     return (
       <>
-        <div className="container-fluid">
-          <div className="row">
+        <div className='container-fluid'>
+          <div className='row'>
             <div
               hidden={!this.props.loading}
-              className="card"
-              style={{ position: "fixed", right: "40%", bottom: "42%", zIndex: "99" }}>
+              className='card'
+              style={{ position: 'fixed', right: '40%', bottom: '42%', zIndex: '99' }}>
             </div>
           </div>
         </div>
-        <div style={{ position: "fixed", right: "14%", bottom: "10px", zIndex: "99" }}>
+        <div style={{ position: 'fixed', right: '14%', bottom: '10px', zIndex: '99' }}>
         </div>
-        <EventFilters />
       </>
     )
   }
