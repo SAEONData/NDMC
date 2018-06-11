8a11b52d537296099eab3472a9994321d7692a5b
Alex <alexconrad@fastmail.com>
2018-06-11 16:55:46 +0200

    WIP(RegionFilter): setup example region filter select

--- NDMC.Web.React/app/js/components/Events/Filters/RegionFilters.jsx
+++ NDMC.Web.React/app/js/components/Events/Filters/RegionFilters.jsx
@@ -13,31 +13,25 @@ import { UILookup } from '../../../constants/ui_config'
 import { stripURLParam, GetUID } from '../../../globalFunctions.js'
 
 //AntD Tree
-import Tree from 'antd/lib/tree'
+import TreeSelect from 'antd/lib/tree-select'
 import Input from 'antd/lib/input'
-import '../../../../css/antd.tree.css' //Overrides default antd.tree css
-const TreeNode = Tree.TreeNode
+//import treestyle from '../../../../css/antd.tree.css' //Overrides default antd.tree css
 
 //GraphQL
 import { graphql } from 'graphql'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
-
 const queryString = require('query-string')
 
 const mapStateToProps = (state, props) => {
-  let { lookupData: { region } } = state
   let { filterData: { regionFilter } } = state
-  return { regionFilter, region }
+  return { regionFilter }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     loadRegionFilter: payload => {
       dispatch({ type: ACTION_TYPES.LOAD_REGION_FILTER, payload })
-    },
-    loadRegions: payload => {
-      dispatch({ type: ACTION_TYPES.LOAD_REGION, payload })
     }
   }
 }
@@ -45,11 +39,11 @@ const mapDispatchToProps = (dispatch) => {
 class RegionFilters extends React.Component {
   constructor(props) {
     super(props)
-    this.expandAllNodes = this.expandAllNodes.bind(this)
-    this.collapseAllNodes = this.collapseAllNodes.bind(this)
     this.onSelect = this.onSelect.bind(this)
-    this.onExpand = this.onExpand.bind(this)
-    this.state = { expandedKeys: [] }
+    this.onChange = this.onChange.bind(this)
+    this.state = {
+      treeValue: undefined
+    }
 
     //Read initial filter from URL
     const parsedHash = queryString.parse(location.hash.replace('/events?', ''))
@@ -62,30 +56,6 @@ class RegionFilters extends React.Component {
   }
 
   componentDidMount() {
-    //Load data
-    let { loadRegions } = this.props
-    fetch(apiBaseURL + 'api/events/regions', {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(res => {
-        loadRegions(res)
-      })
-  }
-
-  renderTreeNodes(data) {
-    return data.map((item) => {
-      if (item.children) {
-        return (
-          <TreeNode title={(item.modifiedState === true ? '* ' : '') + item.text} key={item.id} dataRef={item}>
-            {this.renderTreeNodes(item.children)}
-          </TreeNode>
-        )
-      }
-      return <TreeNode title={(item.modifiedState === true ? '* ' : '') + item.text} key={item.id} />
-    })
   }
 
   onSelect(selectedKeys, info) {
@@ -97,58 +67,20 @@ class RegionFilters extends React.Component {
     loadRegionFilter(id)
   }
 
-  getParentKeys(id, data) {
-    let parentKeys = []
-    if (data.length > 0 && id > 0) {
-      let idKey = Object.keys(data[0])[0].toString()
-      let parentIdKey = 'Parent' + idKey
-      let selectedItem = data.filter(x => x[idKey] == id)[0]
-      if (selectedItem[parentIdKey] !== null) {
-        let parentId = selectedItem[parentIdKey].toString()
-        parentKeys.push(parentId)
-        parentKeys.push(...this.getParentKeys(parentId, data))
-      }
-    }
-    return parentKeys
+  onChange(value) {
+    console.log(value)
+    this.setState({ treeValue: value })
   }
 
-  expandAllNodes() {
-    let expandedKeys = []
-    let { region } = this.props
-    region.map(x => expandedKeys.push(x.RegionId.toString()))
-    this.setState({ expandedKeys: expandedKeys })
-  }
-
-  collapseAllNodes() {
-    this.setState({ expandedKeys: [] })
-  }
-
-  onExpand(expandedKeys) {
-    this.setState({ expandedKeys: expandedKeys })
-  }
-
-  transformDataTree(effectiveData, globalData, level = 0) {
-    let treeNodes = []
-    let parentIdKey = "Parent" + Object.keys(effectiveData[0])[0].toString()
-    if (typeof globalData === 'undefined') {
-      globalData = effectiveData
-    }
-    if (level === 0) {
-      effectiveData = effectiveData.filter(x => x[parentIdKey] === null)
-    }
-    effectiveData.map(item => {
-      let newTreeNode = {
-        id: item[Object.keys(item)[0]],
-        text: item[Object.keys(item)[1]],
-        modifiedState: item.modifiedState
-      }
-      let children = globalData.filter(x => x[parentIdKey] == newTreeNode.id)
-      if (children.length > 0) {
-        newTreeNode.children = this.transformDataTree(children, globalData, (level + 1))
-      }
-      treeNodes.push(newTreeNode)
+  transformDataTree(filteredRegions) {
+    let regions = filteredRegions.map(i => {
+      return { regionName: i.regionName, parentRegionId: i.parentRegionId, regionId: i.regionId, children: [], label: i.regionName, value: `${i.regionId}`, key: i.regionId }
     })
-    return treeNodes
+    regions.forEach(f => { f.children = regions.filter(g => g.parentRegionId == f.regionId) })
+
+    var resultArray = regions.filter(f => f.parentRegionId == null)
+    console.log(resultArray)
+    return resultArray
   }
 
   render() {
@@ -157,13 +89,12 @@ class RegionFilters extends React.Component {
 
     const GET_REGIONS = gql`
       {
-        EventRegions {
-          region {
+        Regions {
             regionName
             regionId
+            parentRegionId
             regionType {
               regionTypeName
-            }
           }
         }
       }`
@@ -176,49 +107,32 @@ class RegionFilters extends React.Component {
 
     return (
       <>
-        <div className='row'>
-          <div className='col-md-12'>
-            <Button color='secondary' size='sm' id='btnRegionTreeExpandAll' style={{ marginLeft: '0px' }} onTouchTap={this.expandAllNodes} >
-              <i className='fa fa-plus-circle' aria-hidden='true'></i>&nbsp;&nbsp;Expand all
-            </Button>
-            <Button color='secondary' size='sm' id='btnRegionTreeCollapseAll' onTouchTap={this.collapseAllNodes}>
-              <i className='fa fa-minus-circle' aria-hidden='true'></i>&nbsp;&nbsp;Collapse all
-            </Button>
-          </div>
-        </div>
         <br />
         {<Query query={GET_REGIONS}>
           {({ data, loading, error }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error Loading Data From Server...</p>
-            console.log(data)
-           // console.log(data.EventRegions.filter(region => { !region.regionName.includes('Ward') }) )
-            let filteredRegions = data.EventRegions.filter(function(region){
-              console.log(region)
-             // !region.regionName.includes('Ward')
-            })
-            //  console.log(filteredRegions)
-            // if (filteredRegions.length > 1) {
-            //   treeData = this.transformDataTree(filteredRegions)
-            // }
-
-            console.log(data)
+            let filteredRegions = data.Regions.filter(region => !region.regionName.includes("Ward"))
+            let regionTree = this.transformDataTree(filteredRegions)
+            console.log(regionTree)
             return (
-              <Tree key={GetUID()}
-                autoExpandParent
-                onSelect={this.onSelect}
-                defaultSelectedKeys={[regionFilter.toString()]}
-                defaultExpandedKeys={[...expandedKeys, ...this.getParentKeys(regionFilter, region), regionFilter.toString()]}
-                onExpand={this.onExpand}
-              >
-                {treeData === undefined ? [] : this.renderTreeNodes(treeData)}
-              </Tree>
+              <div className='row'>
+                  <TreeSelect key={GetUID()}
+                    style={{ width: 300 }}
+                    value={this.state.treeValue}
+                   // dropdownStyle={treestyle}
+                    treeData={this.transformDataTree(regionTree)}
+                    placeholder="Please select a region"
+                    treeDefaultExpandAll
+                    onChange={this.onChange}
+                  >
+                  </TreeSelect>
+              </div>
             )
           }
           }
         </Query>
         }
-        <ReactTooltip />
       </>
     )
   }
