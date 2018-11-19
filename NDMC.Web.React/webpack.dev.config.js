18d308b2db89730cf8eea2518485100cb75ce1c9
terrahop <terra@fastmail.com>
2018-11-19 09:36:35 +0200

    chore(Webpack): allow js less-loader

--- NDMC.Web.React/webpack.dev.config.js
+++ NDMC.Web.React/webpack.dev.config.js
@@ -47,7 +47,12 @@ module.exports = {
       use: [
         'style-loader',
         'css-loader',
-        'less-loader'
+        {
+          loader: 'less-loader',
+          options: {
+            javascriptEnabled: true
+          }
+        }
       ]
     },
     {
