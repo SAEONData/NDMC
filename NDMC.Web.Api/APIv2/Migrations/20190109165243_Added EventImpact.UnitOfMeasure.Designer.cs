8d5fac9e7b739b7cee24aada84a560f4d6946f11
andree1985 <30987911+andree1985@users.noreply.github.com>
2019-01-09 19:05:39 +0200

    - Added migration for latest model changes - Update DB scripts

--- NDMC.Web.Api/APIv2/Migrations/20190109165243_Added EventImpact.UnitOfMeasure.Designer.cs
+++ NDMC.Web.Api/APIv2/Migrations/20190109165243_Added EventImpact.UnitOfMeasure.Designer.cs
@@ -4,14 +4,16 @@ using APIv2.Database.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace APIv2.Migrations
 {
     [DbContext(typeof(SQLDBContext))]
-    partial class SQLDBContextModelSnapshot : ModelSnapshot
+    [Migration("20190109165243_Added EventImpact.UnitOfMeasure")]
+    partial class AddedEventImpactUnitOfMeasure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NDMC.Web.Api/APIv2/Migrations/20190109165243_Added EventImpact.UnitOfMeasure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIv2.Migrations
+{
+    public partial class AddedEventImpactUnitOfMeasure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UnitOfMeasure",
+                table: "EventImpacts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitOfMeasure",
+                table: "EventImpacts");
+        }
+    }
+}
