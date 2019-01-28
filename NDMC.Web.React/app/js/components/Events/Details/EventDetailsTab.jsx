9a28fb94c38c03633d84bb7ef1523bc4bf371647
terrahop <terra@fastmail.com>
2019-01-28 18:44:19 +0200

    fix(EventDetailsTab): update region display to vms

--- NDMC.Web.React/app/js/components/Events/Details/EventDetailsTab.jsx
+++ NDMC.Web.React/app/js/components/Events/Details/EventDetailsTab.jsx
@@ -9,6 +9,8 @@ import TextComponent from '../../Shared/TextComponent.jsx'
 import RangeComponent from '../../Shared/RangeComponent.jsx'
 import OData from 'react-odata'
 import { apiBaseURL } from '../../../config/serviceURLs.js'
+import { vmsBaseURL } from '../../../config/serviceURLs.js'
+
 
 const mapStateToProps = (state, props) => {
   return {}
@@ -19,31 +21,46 @@ const mapStateToProps = (state, props) => {
  * @class
  */
 class EventDetailsTab extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      eventData: {}
+      eventData: {},
+      regionId: '',
+      regionName: '',
     }
+
+    this.getRegion = this.getRegion.bind(this)
   }
 
-  render () {
+  async getRegion() {
+    let fetchURL = `${vmsBaseURL}/regions/${this.state.regionId}`
+    fetch(fetchURL)
+    .then(res => {
+      return res.json()
+    })
+    .then(json => {
+      this.setState({
+        regionName: json.value
+      })
+    })
+  }
+
+  render() {
     const { eventId } = this.props
-    let impactQuery = {
+    let eventQuery = {
       select: ['StartDate', 'EndDate'],
       filter: {
         EventId: parseInt(eventId),
       },
       expand: ['EventRegions/EventImpacts/TypeImpact',
-        {
-          EventRegions:
-          {
-            filter: { Region: { RegionTypeId: { ne: 5 } } },
-            expand: ['Region']
-          }
-        },
         'DeclaredEvents',
         'TypeEvent']
     }
+
+    if(!this.state.regionName){
+      this.getRegion()
+    }
+
     return (
       <>
         <div style={{
@@ -56,16 +73,19 @@ class EventDetailsTab extends React.Component {
           padding: "10px",
           marginTop: "-10px"
         }}>
-          <OData baseUrl={apiBaseURL + 'Events'} query={impactQuery}>
+          <OData baseUrl={apiBaseURL + 'Events'} query={eventQuery}>
             {({ loading, error, data }) => {
               if (loading) { return <div>Loading...</div> }
-              if (error) { return <div>Error Loading Data From Server</div> }
+              if (error) { { console.log(error) } return <div>Error Loading Data From Server</div> }
               if (data && data.value) {
+                let regionId = data.value[0].EventRegions[0].RegionId
+                this.state.regionId = regionId
+
                 const event = data.value[0]
                 let startdate = event.StartDate > 0 ? new Date(event.StartDate * 1000) : 'N/A'
                 let enddate = event.EndDate > 0 ? new Date(event.EndDate * 1000) : 'N/A'
 
-                let declareddate = event.DeclaredEvents[0].DeclaredDate !== null ? 
+                let declareddate = event.DeclaredEvents[0].DeclaredDate !== null ?
                   new Date(event.DeclaredEvents[0].DeclaredDate * 1000) : null
 
                 return (<>
@@ -86,7 +106,7 @@ class EventDetailsTab extends React.Component {
                       col='col-md-6'
                       label='Region'
                       id='txtEventRegion'
-                      value={event.EventRegions[0] ? event.EventRegions[0].Region.RegionName : ''}
+                      value={this.state.regionName ? this.state.regionName : 'N/A'}
                       allowEdit={false}
                     />
                   </div>
@@ -97,8 +117,8 @@ class EventDetailsTab extends React.Component {
                       label='Date'
                       id='txtEventDate'
                       inputWidth='100px'
-                      valueFrom={ startdate !== 'N/A' ?`${startdate.getFullYear()}\/${startdate.getMonth() + 1}\/${startdate.getDate()}` : startdate}
-                      valueTo={ enddate !== 'N/A' ? `${enddate.getFullYear()}\/${enddate.getMonth() + 1}\/${enddate.getDate()}` : enddate}
+                      valueFrom={startdate !== 'N/A' ? `${startdate.getFullYear()}\/${startdate.getMonth() + 1}\/${startdate.getDate()}` : startdate}
+                      valueTo={enddate !== 'N/A' ? `${enddate.getFullYear()}\/${enddate.getMonth() + 1}\/${enddate.getDate()}` : enddate}
                       allowEdit={false}
                     />
                   </div>
